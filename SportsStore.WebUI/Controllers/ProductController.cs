0d784538c9cbe26eddfa4a33269acd7cc6c2c580
Carlo Andaya <carlo@carloandaya.com>
2013-05-18 21:19:58 -0700

    Added pagination to Product page.

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -11,6 +11,7 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         // This method will allow Ninject to inject the dependency for the 
         // product repository when it instantiates the controller class.
@@ -19,9 +20,12 @@ namespace SportsStore.WebUI.Controllers
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
