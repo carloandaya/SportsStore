4b48dd5d23940a45d0cecbde24266f842c545111
Carlo Andaya <carlo@carloandaya.com>
2013-05-18 17:49:28 -0700

    Created IProductRepository and created Mock object.

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using Moq;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -23,7 +26,14 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            // Add bindings here
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Football", Price = 25 },
+                new Product { Name = "Surf board", Price = 179 },
+                new Product { Name = "Running shoes", Price = 95 }
+            }.AsQueryable());
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
