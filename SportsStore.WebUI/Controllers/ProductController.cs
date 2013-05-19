26834fdfada3aed88d90c88c8745cc63b52f61fd
Carlo Andaya <carlo@carloandaya.com>
2013-05-18 18:01:49 -0700

    Created ProductController and List view.

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        // This method will allow Ninject to inject the dependency for the 
+        // product repository when it instantiates the controller class.
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
